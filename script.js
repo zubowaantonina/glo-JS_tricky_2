6b27e127f2a63514ab4d881fd368d215ea99b280
zubowaantonina <zubowaantonina@yandex.ru>
2024-04-05 22:31:57 +0300

    услолжненное задание lesson06

--- script.js
+++ script.js
@@ -1,1 +1,58 @@
 "use strict";
+const playRound = () => {
+    const isNumber = function (num) {
+        return !isNaN(parseFloat(num)) && isFinite(num);
+    };
+    const getNumber = () => Math.ceil(Math.random() * 100);
+    let firstNumber = getNumber();
+    console.log(firstNumber);
+    let walk = 10;
+    const steps = () => {
+        let secondNumber = +prompt("Угадай число от 1 до 100");
+        if (secondNumber === 0) {
+            alert("Пока!");
+        } else {
+            if (walk === 1 && firstNumber !== secondNumber) {
+                let choice = confirm("Попытки закончились, хотите сыграть еще?");
+                if (choice) {
+                    firstNumber = getNumber();
+                    console.log(firstNumber);
+                    walk = 10;
+                    steps();
+                } else {
+                    alert("Пока!");
+                }
+            } else {
+                if (!isNumber(secondNumber)) {
+                    alert("Введи число!");
+                    steps();
+                } else if (secondNumber === null) {
+                    alert("Игра окончена");
+                } else if (firstNumber === secondNumber) {
+                    let result = confirm(
+                        '"Поздравляю, Вы угадали!!! Хотели бы сыграть еще?'
+                    );
+
+                    if (result) {
+                        firstNumber = getNumber();
+                        console.log(firstNumber);
+                        walk = 10;
+                        steps();
+                    } else {
+                        alert("Пока!");
+                    }
+                } else if (firstNumber < secondNumber) {
+                    alert("Загаданное число меньше");
+                    walk--;
+                    steps();
+                } else if (firstNumber > secondNumber) {
+                    alert("Загаданное число больше");
+                    walk--;
+                    steps();
+                }
+            }
+        }
+    };
+    steps();
+};
+playRound();
